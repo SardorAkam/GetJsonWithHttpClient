061d497970b3d0e5eb07eb7981001fdbe371e2e3
SardorAkam <sardorbekibnsadriddin@gmail.com>
2025-04-19 17:11:08 +0500

    Добавьте файлы проекта.

---
+++ Homework/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Customer
+    {
+            public string name { get; set; }
+            public string language { get; set; }
+            public string id { get; set; }
+            public string bio { get; set; }
+            public double version { get; set; }
+    }
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,16 @@
+﻿using Homework;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+namespace HomeWork
+{
+    class Program
+    {
+        public static void Main()
+        {
+            HttpClient httpClient = new HttpClient();
+
+        }
+    }
+} 
